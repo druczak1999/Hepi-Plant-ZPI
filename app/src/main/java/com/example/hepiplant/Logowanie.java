54c41b1a11d330093322002a548cb0d6291472a8
druczak1999 <65281661+druczak1999@users.noreply.github.com>
2021-07-27 20:11:00 +0200

    Add init project

---
+++ app/src/main/java/com/example/hepiplant/Logowanie.java
@@ -0,0 +1,14 @@
+package com.example.hepiplant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Logowanie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logowanie);
+    }
+}
---
+++ app/src/main/java/com/example/hepiplant/Uzytkownik.java
@@ -0,0 +1,27 @@
+package com.example.hepiplant;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.widget.Toolbar;
+
+public class Uzytkownik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uzytkownik);
+        Toolbar myToolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
