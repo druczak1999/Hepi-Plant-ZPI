927f6928a09395c5ba0250c450d97b4f8a70372e
druczak1999 <65281661+druczak1999@users.noreply.github.com>
2021-10-05 12:45:12 +0200

    Add config.properties

---
+++ app/src/main/java/com/example/hepiplant/configuration/Configuration.java
@@ -0,0 +1,50 @@
+package com.example.hepiplant.configuration;
+
+import android.app.Application;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration extends Application {
+    private String url;
+    private long userId;
+    //backend security
+    private String token;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String readProperties() throws IOException {
+
+        Properties prop = new Properties();
+        AssetManager assetManager = getApplicationContext().getAssets();
+        InputStream inputStream = assetManager.open("config.properties");
+        prop.load(inputStream);
+        return prop.getProperty("url");
+    }
+}
