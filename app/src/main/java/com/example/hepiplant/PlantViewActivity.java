e4010b560e4b810f1173d73091e9ad0a58287e9f
druczak1999 <65281661+druczak1999@users.noreply.github.com>
2021-10-12 20:00:17 +0200

    Add category name to plant view

--- app/src/main/java/com/example/hepiplant/PlantViewActivity.java
+++ app/src/main/java/com/example/hepiplant/PlantViewActivity.java
@@ -1,19 +1,44 @@
 package com.example.hepiplant;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.hepiplant.configuration.Configuration;
+import com.example.hepiplant.dto.CategoryDto;
+import com.example.hepiplant.dto.PlantDto;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class PlantViewActivity extends AppCompatActivity {
 
     TextView plantName, species, category, watering, fertilizing, misting, soil, location, placement, date;
     ImageView plantImage;
+    String categoryName = null;
+    private Configuration config;
+    private static final String TAG = "PlantViewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +48,7 @@ public class PlantViewActivity extends AppCompatActivity {
     }
 
     private void setupViewsData(){
+        config = (Configuration) getApplicationContext();
         plantName = findViewById(R.id.PlantName);
         species = findViewById(R.id.SpeciesValueView);
         category = findViewById(R.id.CategoryValueView);
@@ -59,7 +85,13 @@ public class PlantViewActivity extends AppCompatActivity {
     private void setTextsToRealValues(){
         plantName.setText(getIntent().getExtras().getString("plantName"));
         species.setText(getIntent().getExtras().getString("species"));
-        category.setText(getIntent().getExtras().getString("category"));
+        if(getIntent().getExtras().getString("category")!=null && !getIntent().getExtras().getString("category").isEmpty()){
+            getCategoryName(Integer.parseInt(Objects.requireNonNull(getIntent().getExtras().getString("category"))));
+
+        }
+        else{
+            category.setText("");
+        }
         watering.setText("Co "+getIntent().getExtras().getString("watering")+" dni");
         fertilizing.setText("Co "+getIntent().getExtras().getString("fertilizing")+" dni");
         misting.setText("Co "+getIntent().getExtras().getString("misting")+" dni");
@@ -70,5 +102,57 @@ public class PlantViewActivity extends AppCompatActivity {
         if(!getIntent().getExtras().getString("photo").isEmpty())
         plantImage.setImageURI(Uri.parse(getIntent().getExtras().getString("photo")));
 
+    }
+
+    private void getCategoryName(int id){
+        makeGetDataRequest(id);
+    }
+
+    private void makeGetDataRequest(int id){
+        String url = getRequestUrl(id);
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        categoryName = onGetResponseReceived(response);
+                        Log.v(TAG,"In set categoyr "+categoryName);
+                        if(categoryName!=null)
+                        category.setText(categoryName);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                onErrorResponseReceived(error);
+            }
+        });
+        Log.v(TAG, "Sending the request to " + url);
+        config.getQueue().add(jsonArrayRequest);
+    }
+
+    @NonNull
+    private String getRequestUrl(int id) {
+        try {
+            config.setUrl(config.readProperties());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return config.getUrl() + "categories/"+id;
+    }
+
+    private String onGetResponseReceived(JSONObject response){
+        Log.v(TAG, "onGetResponseReceived()");
+        Gson gson = new Gson();
+        CategoryDto categoryDto = gson.fromJson(String.valueOf(response), CategoryDto.class);
+        Log.v(TAG,categoryDto.getName());
+        return categoryDto.getName();
+    }
+
+    private void onErrorResponseReceived(VolleyError error){
+        Log.e(TAG, "Request unsuccessful. Message: " + error.getMessage());
+        NetworkResponse networkResponse = error.networkResponse;
+        if (networkResponse != null) {
+            Log.e(TAG, "Status code: " + String.valueOf(networkResponse.statusCode) + " Data: " + networkResponse.data);
+        }
     }
 }
